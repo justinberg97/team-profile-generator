b50b66d2b2f0b85446580beea21a5f023ec1b249
justinberg <justinmberg97@gmail.com>
2022-11-21 17:25:47 -0600

    more tests

--- Employee.js
+++
@@ -1,9 +0,0 @@
-class Employee {
-    constructor(name, employee_id, email) {
-        this.name = name;
-        this.employee_id = employee_id;
-        this.email = email;
-    }
-}
-
-module.exports = Employee;
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,4 +1,4 @@
-const Employee = require('../Employee');
+const Employee = require('../lib/Employee');
 
 test('Can create a new employee object', () => {
     const employee = new Employee();
@@ -7,6 +7,24 @@ test('Can create a new employee object', () => {
 
 
 
+test('Can set name in employee object', () => {
+    const employee = new Employee("Justin");
+    expect(employee.name).toBe("Justin")
+});
+
+test('Can set employee object_id in employee object', () => {
+    const employee = new Employee("Justin", 13);
+    expect(employee.employee_id).toBe(13);
+});
 
 
+test('Can set email in employee object', () => {
+    const employee = new Employee("Justin", 13, "justin@gmail.com");
+    expect(employee.email).toBe("justin@gmail.com");
+});
 
+
+test('Can get name from getName() method', () => {
+    const employee = new Employee("Justin", 13, "justin@gmail.com")
+    expect(employee.getName()).toBe("Justin")
+})
